6a3fa70a4f3283237b35b06edc7500425434b919
Emily Foley <foleyemilym@gmail.com>
2014-07-01 17:36:07 -0700

    adding marker to Salt Lake City in js file

--- maps.js
+++ maps.js
@@ -5,5 +5,19 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
-}
+  var marker = new google.maps.Marker({
+      position: center,
+      map: map,
+      title:"Salt Lake City!"
+  });
+  marker.setMap(map);
+};
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
