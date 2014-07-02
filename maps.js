8aa836006ab6ea7a6f88ac7115498dd479aae45d
Emily Foley <foleyemilym@gmail.com>
2014-07-01 17:42:24 -0700

    marker successfully added

--- maps.js
+++ maps.js
@@ -1,19 +1,22 @@
 function initialize() {
+  var location = new google.maps.LatLng(40.7607793, -111.8910474)
   var mapOptions = {
-    center: new google.maps.LatLng(40.7607793, -111.8910474),
+    center: location,
     zoom: 6
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
   var marker = new google.maps.Marker({
-      position: center,
+      position: location,
       map: map,
-      title:"Salt Lake City!"
+      title:"Salt Lake City!",
+      visible: true
   });
   marker.setMap(map);
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
 
 
