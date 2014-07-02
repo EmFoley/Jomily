3f674c61d8156d2ffcbe0eb3a95e7157097f700b
Emily Foley <foleyemilym@gmail.com>
2014-07-01 18:32:40 -0700

    refactoring, version control

--- maps.js
+++ maps.js
@@ -11,15 +11,14 @@ function initialize() {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-// function allThePlaces() {
-
-// }
 
 function markerMaker() {
   var placesList = [
     (40.7607793, -111.8910474),
     (40.7607793, -110.8910474)
   ]
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
     for(i=0, i < placesList.length, i++) {
       var marker = new google.maps.Marker({
           position: i,
