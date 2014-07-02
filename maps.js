aba2d33a564fb6a350ccb5bf3ae337f4100a923f
Emily Foley <foleyemilym@gmail.com>
2014-07-01 18:19:18 -0700

    refactoring and adding multiple locations function

--- maps.js
+++ maps.js
@@ -6,6 +6,19 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+ markerMaker();
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function allThePlaces() {
+  var placesList = [
+    (40.7607793, -111.8910474),
+
+  ]
+}
+
+function markerMaker() {
   var marker = new google.maps.Marker({
       position: location,
       map: map,
@@ -14,11 +27,6 @@ function initialize() {
   });
   marker.setMap(map);
 };
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
 
 
 
