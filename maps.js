b16e88aec38492861bed30e81aa6dfc8a841be7b
Emily Foley <foleyemilym@gmail.com>
2014-08-11 13:37:15 -0400

    fixes to variable scope

--- maps.js
+++ maps.js
@@ -1,4 +1,8 @@
-var location = new google.maps.LatLng(40.7607793, -111.8910474);
+var location = new google.maps.LatLng(40.7607793,-111.8910474);
+var placesList = [
+    [40.7607793, -111.8910474],
+    [40.7607793, -110.8910474]
+  ];
 var map;
 
 function initialize() {
@@ -6,22 +10,20 @@ function initialize() {
     center: location,
     zoom: 6
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
  markerMaker();
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-var placesList = [
-    [40.7607793, -111.8910474],
-    [40.7607793, -110.8910474]
-  ];
+
 function markerMaker() {
     for(i=0; i < placesList.length; i++) {
       var marker = new google.maps.Marker({
+          map:map,
           position: new google.maps.LatLng(placesList[i].toString()),
-          visible: true
+          visible:true
       });
       marker.setMap(map);
     };
