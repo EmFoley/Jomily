1f6f3544206aaac3145413aee28b18d0ef342b55
Emily Foley <foleyemilym@gmail.com>
2014-08-11 14:59:08 -0400

    multiple location markers loop functioning

--- maps.js
+++ maps.js
@@ -1,29 +1,32 @@
-var location = new google.maps.LatLng(40.7607793,-111.8910474);
-var placesList = [
-    [40.7607793, -111.8910474],
-    [40.7607793, -110.8910474]
-  ];
-var map;
-
 function initialize() {
+  var location = new google.maps.LatLng(40.7607793,-111.8910474);
   var mapOptions = {
     center: location,
     zoom: 6
   };
   map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
- markerMaker();
+  markerMaker();
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+var placesList = [
+  ['Salt Lake City', 40.7607793, -111.8910474],
+  ['Zion National Park', 37.322817, -113.045716]
+]
+// var bounds = new google.maps.LatLngBounds();
+
 
 function markerMaker() {
     for(i=0; i < placesList.length; i++) {
+      var position = new google.maps.LatLng(placesList[i][1], placesList[i][2]);
+      // bounds.extend(position);
       var marker = new google.maps.Marker({
           map:map,
-          position: new google.maps.LatLng(placesList[i].toString()),
-          visible:true
+          position: position,
+          title: placesList[i][0],
+          visible: true
       });
       marker.setMap(map);
     };
